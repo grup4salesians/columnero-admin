49a53aaeb3217c6aa4d8c0917bfb316bc2e6a950
vektrom <kenzo_aku@hotmail.com>
2015-05-21 17:07:40 +0200

    Sleeping Owl

--- app/config/database.php
+++ app/config/database.php
@@ -54,10 +54,10 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => 'forge',
-			'password'  => '',
+			'host'      => 'jona-pc',
+			'database'  => 'dawtrello',
+			'username'  => 'jona',
+			'password'  => '12345',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
